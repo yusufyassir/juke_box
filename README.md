ee5ec0685cf30d947ac4ceae3976f591e082b46c
yusufyassir <yusufyassir0@gmail.com>
2022-10-09 20:43:24 +0300

    base code for df player

---
+++ README.md
@@ -0,0 +1,1 @@
+9/10/2022 - started working on the juke box starting with a base code for the mp3player
---
+++ juke_box_code/juke_box_code.ino
@@ -0,0 +1,31 @@
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+int songnum;
+
+void setup() {
+  // put your setup code here, to run once:
+ mySoftwareSerial.begin(9600);
+ myDFPlayer.begin(mySoftwareSerial);
+ myDFPlayer.volume(10);
+ //myDFPlayer.play(17);
+ Serial.begin(19200);
+ 
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  songnum = Serial.parseInt();
+  Serial.println(songnum);
+  if (songnum == 24){
+        myDFPlayer.play(24);
+  }
+  else if (songnum == 6){
+        myDFPlayer.play(6);                
+  }
+}
