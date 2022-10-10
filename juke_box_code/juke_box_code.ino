bc6378223bf4f0122e9cba580f08892bb330a165
yusufyassir <yusufyassir0@gmail.com>
2022-10-10 06:16:23 +0300

    merge

--- juke_box_code/juke_box_code.ino
+++ juke_box_code/juke_box_code.ino
@@ -2,6 +2,13 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
 SoftwareSerial mySoftwareSerial(2, 3); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
@@ -14,19 +21,46 @@ void setup() {
  myDFPlayer.volume(25);
  
 // myDFPlayer.play(17);
- Serial.begin(19200);
+   Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
  
  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  songnum = Serial.parseInt();
-  Serial.println(songnum);
-  if (songnum == 24){
-        myDFPlayer.play(24);
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
   }
-  else if (songnum == 6){
-        myDFPlayer.play(6);                
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "D1 34 D0 32") //change here the UID of the card/cards that you want to give access
+  {
+      myDFPlayer.play(7);
+  }
+ 
+ else if (content.substring(1) == "F4 97 02 39")   {
+      myDFPlayer.play(14);
   }
 }
